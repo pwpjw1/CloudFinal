0d12b6d25ea2b572945ef77de8f7c42695ccc1e5
pwpjw1 <pwpjw6@gmail.com>
2021-02-26 21:14:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const searchClient = algoliasearch('3VTZA3YCNP', 'cc436a662165dd650ed2ea1c775b8327');
 
+const search = instantsearch({
+indexName: 'Email-Index',
+searchClient,
+});
+
+search.addWidgets([
+instantsearch.widgets.searchBox({
+container: '#searchbox',
+}),
+
+instantsearch.widgets.configure({
+hitsPerPage: 16
+}),
+
+instantsearch.widgets.hits({
+container: '#hits',
+templates: {
+item: `
+<div>
+<div class="hit-name">
+{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+</div>
+</div>
+`,
+},
+}),
+
+instantsearch.widgets.pagination({
+container: '#pagination',
+}),
+]);
+
+search.start();
